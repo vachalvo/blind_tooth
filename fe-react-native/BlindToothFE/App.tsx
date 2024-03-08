5c719977af076f89bdb6e92fa2ac7c54f85d6186
Vojtech Vachal <vachal.vojta@email.cz>
2024-03-09 00:10:42 +0100

    Ble - success

--- fe-react-native/BlindToothFE/App.tsx
+++ fe-react-native/BlindToothFE/App.tsx
@@ -8,6 +8,8 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  PermissionsAndroid,
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -24,10 +26,80 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { BleManager } from "react-native-ble-plx";
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
+
+export const manager = new BleManager();
+
+async function requestBluetoothPermission() {
+  if (Platform.OS === "ios") {
+    return true;
+  }
+  if (
+      Platform.OS === "android" &&
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+  ) {
+    const apiLevel = parseInt(Platform.Version.toString(), 10);
+
+    if (apiLevel < 31) {
+      const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      );
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    }
+    if (
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN &&
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
+    ) {
+      const result = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ]);
+
+      return (
+          result["android.permission.BLUETOOTH_CONNECT"] ===
+          PermissionsAndroid.RESULTS.GRANTED &&
+          result["android.permission.BLUETOOTH_SCAN"] ===
+          PermissionsAndroid.RESULTS.GRANTED &&
+          result["android.permission.ACCESS_FINE_LOCATION"] ===
+          PermissionsAndroid.RESULTS.GRANTED
+      );
+    }
+  }
+
+  // this.showErrorToast("Permission have not been granted");
+
+  return false;
+}
+
+function scanAndConnect() {
+  const permission = requestBluetoothPermission();
+  console.log(permission)
+  if (!permission) {
+    return;
+  }
+
+  manager.startDeviceScan(null, null, (error, device) => {
+    if (error) {
+      // Handle error (scanning will be stopped automatically)
+      return;
+    }
+
+    // Check if it is a device you are looking for based on advertisement data
+    // or other criteria.
+    console.log("device", device);
+    if (device?.name === "TI BLE Sensor Tag" || device?.name === "SensorTag") {
+      // Stop scanning as it's not necessary if you are scanning for one device.
+      manager.stopDeviceScan();
+
+      // Proceed with connection.
+    }
+  });
+}
 
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -58,19 +130,24 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  React.useEffect(() => {
+    const subscription = manager.onStateChange(state => {
+      if (state === 'PoweredOn') {
+        scanAndConnect()
+        subscription.remove()
+      }
+    }, true)
+    return () => subscription.remove()
+  }, [manager])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+        >
         <Header />
         <View
           style={{
