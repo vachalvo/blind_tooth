cae2df96c07b1a81ff0c413795318d128b240cbd
Vojtech Vachal <vachal.vojta@email.cz>
2024-03-09 21:31:10 +0100

    Compass

--- fe/blind-tooth-fe/app/(tabs)/navigation.tsx
+++ fe/blind-tooth-fe/app/(tabs)/navigation.tsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from "react";
 import {
-  Image,
-  View,
-  Dimensions,
-  TouchableOpacity,
-  ScrollView,
+    Image,
+    View,
+    Dimensions, ScrollView,
 } from "react-native";
 import { Grid, Col, Row } from "react-native-easy-grid";
 import { Magnetometer } from "expo-sensors";
@@ -12,13 +10,13 @@ import { Magnetometer } from "expo-sensors";
 import * as NetInfo from "@react-native-community/netinfo";
 import Calls from "@/utils/api/client";
 import * as Location from "expo-location";
-import { Container } from "@/components/Container";
 import { Text } from "react-native-paper";
-import { vibrateLong, vibrateMedium, vibrateShort } from "@/utils/vibrations";
+import { vibrateLong, vibrateShort } from "@/utils/vibrations";
 import { ResponseData, useCache } from "@/utils/cache";
 import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 import { REGISTRATION_KEY } from "../register";
 import { Redirect } from "expo-router";
+import MagnetometerUtils from "@/utils/magnetometerUtils";
 
 const { height, width } = Dimensions.get("window");
 
@@ -150,95 +148,40 @@ export default function App() {
     return () => clearInterval(interval);
   }, [userId, cache, setLatestData]);
 
-  const [magnetometer, setMagnetometer] = useState(0);
-  const [subscription, setSubscription] = useState<any>(null);
+    const [subscription, setSubscription] = useState<any>(null);
+    const [magnetometer, setMagnetometer] = useState<any>(0);
 
   useEffect(() => {
-    getItem().then((res) => {
+      Magnetometer.setUpdateInterval(1000)
+
+      getItem().then((res) => {
       setUserId(res);
       setLoading(false);
     });
   }, []);
 
+    const _subscribe = () => {
+        setSubscription(
+            Magnetometer.addListener((data) => {
+                setMagnetometer(MagnetometerUtils.getAngle(data));
+            })
+        );
+    };
+
+    const _unsubscribe = () => {
+        subscription && subscription.remove();
+        setSubscription(null);
+    };
+
+    useEffect(() => {
+        _subscribe();
+
+        return () => _unsubscribe();
+    }, []);
+
   if (loading) return <Text>Loading...</Text>;
 
   if (userId === null) return <Redirect href={"/register"} />;
-
-  const _slow = () => Magnetometer.setUpdateInterval(1000);
-  const _fast = () => Magnetometer.setUpdateInterval(10000);
-
-  // const _subscribe = () => {
-  //   Magnetometer.isAvailableAsync().then((available) => {
-  //     if (!available) {
-  //       Magnetometer.requestPermissionsAsync().then((status) => {
-  //         if (!status.granted) {
-  //           alert("Permission to access location was denied");
-  //         }
-
-  //         setSubscription(
-  //           Magnetometer.addListener((MagnetometerData) => {
-  //             setData(MagnetometerData);
-  //           })
-  //         );
-  //       });
-  //     }
-  //   });
-
-  //   setSubscription(
-  //     Magnetometer.addListener((MagnetometerData) => {
-  //       setData(MagnetometerData);
-  //       setMagnetometer(_angle(MagnetometerData));
-  //     })
-  //   );
-  // };
-
-  // const _unsubscribe = () => {
-  //   subscription && subscription.remove();
-  //   setSubscription(null);
-  // };
-
-  // useEffect(() => {
-  //   _subscribe();
-  //   return () => _unsubscribe();
-  // }, []);
-
-  // const _angle = (magnetometer: any) => {
-  //   let angle = 0;
-  //   if (magnetometer) {
-  //     let { x, y, z } = magnetometer;
-  //     if (Math.atan2(y, x) >= 0) {
-  //       angle = Math.atan2(y, x) * (180 / Math.PI);
-  //     } else {
-  //       angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
-  //     }
-  //   }
-  //   return Math.round(angle);
-  // };
-
-  // const _direction = (degree: any) => {
-  //   if (degree >= 22.5 && degree < 67.5) {
-  //     return "NE";
-  //   } else if (degree >= 67.5 && degree < 112.5) {
-  //     return "E";
-  //   } else if (degree >= 112.5 && degree < 157.5) {
-  //     return "SE";
-  //   } else if (degree >= 157.5 && degree < 202.5) {
-  //     return "S";
-  //   } else if (degree >= 202.5 && degree < 247.5) {
-  //     return "SW";
-  //   } else if (degree >= 247.5 && degree < 292.5) {
-  //     return "W";
-  //   } else if (degree >= 292.5 && degree < 337.5) {
-  //     return "NW";
-  //   } else {
-  //     return "N";
-  //   }
-  // };
-
-  // // Match the device top with pointer 0° degree. (By default 0° starts from the right of the device.)
-  // const _degree = (magnetometer: any) => {
-  //   return magnetometer - 90 >= 0 ? magnetometer - 90 : magnetometer + 271;
-  // };
 
   const direction =
     (latestData?.newSignalAvg ?? 0) - (latestData?.oldSignalAvg ?? 0) >= 0
@@ -254,83 +197,55 @@ export default function App() {
   }
 
   return (
-    <Container>
-      <ScrollView>
-        <Text variant="bodySmall">{JSON.stringify(latestData)}</Text>
+     <Grid style={{backgroundColor: "yellow"}}>
+         <ScrollView>
+             <Text variant="bodySmall">{JSON.stringify(latestData)}</Text>
 
-        <Text variant="bodyLarge">
-          {direction === "good" ? "Vedeš si dobře" : "Moc ti to nejde"}
-        </Text>
+             <Text variant="bodyLarge">
+                 {direction === "good" ? "Vedeš si dobře" : "Moc ti to nejde"}
+             </Text>
 
-        <Text variant="bodyLarge">
-          Síla signálu: {latestData?.newSignalAvg}
-        </Text>
-      </ScrollView>
-    </Container>
-    // <Grid style={{ backgroundColor: "black" }}>
-    //   <Row style={{ alignItems: "center" }} size={0.9}>
-    //     <Col style={{ alignItems: "center" }}>
-    //       <Text
-    //         style={{
-    //           color: "#fff",
-    //           fontSize: height / 26,
-    //           fontWeight: "bold",
-    //         }}
-    //       >
-    //         {_direction(_degree(magnetometer))}
-    //       </Text>
-    //     </Col>
-    //   </Row>
+             <Text variant="bodyLarge">
+                 Síla signálu: {latestData?.newSignalAvg}
+             </Text>
+         </ScrollView>
 
-    //   <Row style={{ alignItems: "center" }} size={0.1}>
-    //     <Col style={{ alignItems: "center" }}>
-    //       <View
-    //         style={{
-    //           position: "absolute",
-    //           width: width,
-    //           alignItems: "center",
-    //           top: 0,
-    //         }}
-    //       >
-    //         <Image
-    //           source={require("../../assets/images/compass_pointer.png")}
-    //           style={{
-    //             height: height / 26,
-    //             resizeMode: "contain",
-    //           }}
-    //         />
-    //       </View>
-    //     </Col>
-    //   </Row>
+         <Row style={{ alignItems: 'center' }} size={.1}>
+             <Col style={{ alignItems: 'center' }}>
+                 <View style={{ position: 'absolute', width: width, alignItems: 'center', top: 0 }}>
+                     <Image source={require("../../assets/images/compass_pointer.png")} style={{
+                         height: height / 26,
+                         resizeMode: 'contain'
+                     }} />
+                 </View>
+             </Col>
+         </Row>
 
-    //   <Row style={{ alignItems: "center" }} size={2}>
-    //     <Text
-    //       style={{
-    //         color: "#fff",
-    //         fontSize: height / 27,
-    //         width: width,
-    //         position: "absolute",
-    //         textAlign: "center",
-    //       }}
-    //     >
-    //       {_degree(magnetometer)}°
-    //     </Text>
+         <Row style={{ alignItems: 'center' }} size={2}>
+             <Text style={{
+                 color: '#fff',
+                 fontSize: height / 27,
+                 width: width,
+                 position: 'absolute',
+                 textAlign: 'center'
+             }}>
+                 {MagnetometerUtils.getDegree(magnetometer)}°
+             </Text>
 
-    //     <Col style={{ alignItems: "center" }}>
-    //       <Image
-    //         source={require("../../assets/images/compass_bg.png")}
-    //         style={{
-    //           height: width - 80,
-    //           justifyContent: "center",
-    //           alignItems: "center",
-    //           resizeMode: "contain",
-    //           transform: [{ rotate: 360 - magnetometer + "deg" }],
-    //         }}
-    //       />
-    //     </Col>
-    //   </Row>
+             <Col style={{ alignItems: 'center' }}>
 
-    //   <Row style={{ alignItems: "center" }} size={1}></Row>
-    // </Grid>
+                 <Image source={require("../../assets/images/compass_bg.png")} style={{
+                     height: width - 80,
+                     justifyContent: 'center',
+                     alignItems: 'center',
+                     resizeMode: 'contain',
+                     transform: [{ rotate: 360 - magnetometer + 'deg' }]
+                 }} />
+
+             </Col>
+         </Row>
+
+        <Row style={{alignItems: "center"}} size={1}></Row>
+      </Grid>
   );
 }
---
+++ fe/blind-tooth-fe/utils/magnetometerUtils.ts
@@ -0,0 +1,36 @@
+const MagnetometerUtils = {
+    getAngle: (magnetometer: any) => {
+        let angle = 0;
+        if (magnetometer) {
+            let { x, y, z } = magnetometer;
+            if (Math.atan2(y, x) >= 0) {
+                angle = Math.atan2(y, x) * (180 / Math.PI);
+            } else {
+                angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
+            }
+        }
+        return Math.round(angle);
+        },
+    getDirectionLevel: (currentDegree: number, targetDegree: number) => {
+        const normalizeDeg = Math.abs(currentDegree - targetDegree);
+
+        if (normalizeDeg >= 22.5 && normalizeDeg < 90) {
+            return 1;
+        } else if (normalizeDeg >= 90 && normalizeDeg < 157.5) {
+            return 2;
+        } else if (normalizeDeg >= 157.5 && normalizeDeg < 205.5) {
+            return 3;
+        } else if (normalizeDeg >= 205.5 && normalizeDeg < 270) {
+            return -2;
+        } else if (normalizeDeg >= 270 && normalizeDeg < 337.5) {
+            return -1;
+        } else {
+            return 0;
+        }
+    },
+    getDegree: (magnetometer: any) => {
+        return magnetometer - 90 >= 0 ? magnetometer - 90 : magnetometer + 271;
+    }
+}
+
+export default MagnetometerUtils
