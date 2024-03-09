4bf64acd25d5b652badb0f5b5b5af327a527f5ea
David Markov <Markov.David@seznam.cz>
2024-03-09 08:42:19 +0100

    working network state subscription

--- fe/blind-tooth-fe/app/(tabs)/index.tsx
+++ fe/blind-tooth-fe/app/(tabs)/index.tsx
@@ -3,93 +3,144 @@ import { PermissionsAndroid, Platform, StyleSheet } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-import { BleManager } from "react-native-ble-plx";
-import { useEffect } from "react";
+// import { BleManager } from "react-native-ble-plx";
+import { useEffect, useState } from "react";
 
-export const manager = new BleManager();
+import * as Network from "expo-network";
 
-async function requestBluetoothPermission() {
-  if (Platform.OS === "ios") {
-    return true;
-  }
-  if (
-    Platform.OS === "android" &&
-    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-  ) {
-    const apiLevel = parseInt(Platform.Version.toString(), 10);
+import {
+  addEventListener,
+  useNetInfo,
+  fetch as fetchNetInfo,
+  configure,
+} from "@react-native-community/netinfo";
 
-    if (apiLevel < 31) {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-      );
-      return granted === PermissionsAndroid.RESULTS.GRANTED;
-    }
-    if (
-      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN &&
-      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
-    ) {
-      const result = await PermissionsAndroid.requestMultiple([
-        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
-        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      ]);
+import * as NetInfo from "@react-native-community/netinfo";
 
-      return (
-        result["android.permission.BLUETOOTH_CONNECT"] ===
-          PermissionsAndroid.RESULTS.GRANTED &&
-        result["android.permission.BLUETOOTH_SCAN"] ===
-          PermissionsAndroid.RESULTS.GRANTED &&
-        result["android.permission.ACCESS_FINE_LOCATION"] ===
-          PermissionsAndroid.RESULTS.GRANTED
-      );
-    }
-  }
+// configure({
+//   reachabilityShortTimeout: 5 * 1000,
+//   reachabilityLongTimeout: 60 * 1000,
+// });
 
-  alert("Permission have not been granted");
-  // this.showErrorToast("Permission have not been granted");
+// Subscribe
+// const unsubscribe = addEventListener((state) => {
+//   console.log("Connection type", state.type);
+//   console.log("Is connected?", state.isConnected);
+//   console.log(state.details);
+// });
 
-  return false;
-}
+// Unsubscribe
+// unsubscribe();
 
-function scanAndConnect() {
-  const permission = requestBluetoothPermission();
-  console.log(permission)
-  if (!permission) {
-    return;
-  }
+// export const manager = new BleManager();
 
-    manager.startDeviceScan(null, null, (error, device) => {
-    if (error) {
-      // Handle error (scanning will be stopped automatically)
-      return;
-    }
+// async function requestBluetoothPermission() {
+//   if (Platform.OS === "ios") {
+//     return true;
+//   }
+//   if (
+//     Platform.OS === "android" &&
+//     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+//   ) {
+//     const apiLevel = parseInt(Platform.Version.toString(), 10);
 
-    // Check if it is a device you are looking for based on advertisement data
-    // or other criteria.
-    console.log(device);
-    if (device?.name === "TI BLE Sensor Tag" || device?.name === "SensorTag") {
-      // Stop scanning as it's not necessary if you are scanning for one device.
-      manager.stopDeviceScan();
+//     if (apiLevel < 31) {
+//       const granted = await PermissionsAndroid.request(
+//         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+//       );
+//       return granted === PermissionsAndroid.RESULTS.GRANTED;
+//     }
+//     if (
+//       PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN &&
+//       PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
+//     ) {
+//       const result = await PermissionsAndroid.requestMultiple([
+//         PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+//         PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+//         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+//       ]);
 
-      // Proceed with connection.
-    }
-  });
-}
+//       return (
+//         result["android.permission.BLUETOOTH_CONNECT"] ===
+//           PermissionsAndroid.RESULTS.GRANTED &&
+//         result["android.permission.BLUETOOTH_SCAN"] ===
+//           PermissionsAndroid.RESULTS.GRANTED &&
+//         result["android.permission.ACCESS_FINE_LOCATION"] ===
+//           PermissionsAndroid.RESULTS.GRANTED
+//       );
+//     }
+//   }
+
+//   alert("Permission have not been granted");
+//   // this.showErrorToast("Permission have not been granted");
+
+//   return false;
+// }
+
+// function scanAndConnect() {
+//   const permission = requestBluetoothPermission();
+//   console.log(permission);
+//   if (!permission) {
+//     return;
+//   }
+
+//   manager.startDeviceScan(null, null, (error, device) => {
+//     if (error) {
+//       // Handle error (scanning will be stopped automatically)
+//       return;
+//     }
+
+//     // Check if it is a device you are looking for based on advertisement data
+//     // or other criteria.
+//     console.log(device);
+//     if (device?.name === "TI BLE Sensor Tag" || device?.name === "SensorTag") {
+//       // Stop scanning as it's not necessary if you are scanning for one device.
+//       manager.stopDeviceScan();
+
+//       // Proceed with connection.
+//     }
+//   });
+// }
 
 export default function TabOneScreen() {
-  useEffect(() => {
-    requestBluetoothPermission();
-  }, []);
+  // useEffect(() => {
+  //   requestBluetoothPermission();
+  // }, []);
+
+  const [netInfo, setNetInfo] = useState<unknown>();
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const subscription = manager.onStateChange((state) => {
-      if (state === "PoweredOn") {
-        scanAndConnect();
-        subscription.remove();
-      }
-    }, true);
-    return () => subscription.remove();
+    const interval = setInterval(() => {
+      NetInfo.refresh()
+        .then((state) => {
+          console.log("Connection type", state.type);
+          console.log("Is connected?", state.isConnected);
+          console.log(state.details);
+          setNetInfo(() => state);
+          setCount((prev) => prev + 1);
+        })
+        .catch((err) => console.error(err));
+    }, 5000);
+
+    return () => clearInterval(interval);
   }, []);
+
+  // useEffect(() => {
+  //   const unsubscribe = addEventListener((state) => {
+  //     console.log("Connection type", state.type);
+  //     console.log("Is connected?", state.isConnected);
+  //     console.log(state.details);
+
+  //     setNetInfo(() => state);
+  //   });
+
+  //   return unsubscribe;
+  // }, [setNetInfo]);
+
+  // const { details } = useNetInfo();
+
+  // console.log(details);
 
   return (
     <View style={styles.container}>
@@ -100,6 +151,11 @@ export default function TabOneScreen() {
         darkColor="rgba(255,255,255,0.1)"
       />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {(netInfo as any) && (
+        <Text>{JSON.stringify(netInfo as any, null, 2)}</Text>
+      )}
+
+      {count > 0 && <Text>{count}</Text>}
     </View>
   );
 }
