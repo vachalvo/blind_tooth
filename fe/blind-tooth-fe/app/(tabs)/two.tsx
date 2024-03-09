b768fd3d610739545b50bf38dadc260d39ea9709
David Markov <Markov.David@seznam.cz>
2024-03-09 09:10:19 +0100

    x

--- fe/blind-tooth-fe/app/(tabs)/two.tsx
+++ fe/blind-tooth-fe/app/(tabs)/two.tsx
@@ -1,14 +1,123 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { useEffect, useState } from "react";
 
-export default function TabTwoScreen() {
+import { Magnetometer } from "expo-sensors";
+
+export default function App() {
+  const [{ x, y, z }, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [magnetometer, setMagnetometer] = useState(0);
+  const [subscription, setSubscription] = useState<any>(null);
+
+  const _slow = () => Magnetometer.setUpdateInterval(1000);
+  const _fast = () => Magnetometer.setUpdateInterval(16);
+
+  const _subscribe = () => {
+    Magnetometer.isAvailableAsync().then((available) => {
+      if (!available) {
+        Magnetometer.requestPermissionsAsync().then((status) => {
+          if (!status.granted) {
+            alert("Permission to access location was denied");
+          }
+
+          setSubscription(
+            Magnetometer.addListener((MagnetometerData) => {
+              setData(MagnetometerData);
+            })
+          );
+        });
+      }
+    });
+
+    setSubscription(
+      Magnetometer.addListener((MagnetometerData) => {
+        setData(MagnetometerData);
+        setMagnetometer(_angle(MagnetometerData));
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const _angle = (magnetometer: any) => {
+    let angle = 0;
+    if (magnetometer) {
+      let { x, y, z } = magnetometer;
+      if (Math.atan2(y, x) >= 0) {
+        angle = Math.atan2(y, x) * (180 / Math.PI);
+      } else {
+        angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
+      }
+    }
+    return Math.round(angle);
+  };
+
+  const _direction = (degree: any) => {
+    if (degree >= 22.5 && degree < 67.5) {
+      return "NE";
+    } else if (degree >= 67.5 && degree < 112.5) {
+      return "E";
+    } else if (degree >= 112.5 && degree < 157.5) {
+      return "SE";
+    } else if (degree >= 157.5 && degree < 202.5) {
+      return "S";
+    } else if (degree >= 202.5 && degree < 247.5) {
+      return "SW";
+    } else if (degree >= 247.5 && degree < 292.5) {
+      return "W";
+    } else if (degree >= 292.5 && degree < 337.5) {
+      return "NW";
+    } else {
+      return "N";
+    }
+  };
+
+  // Match the device top with pointer 0° degree. (By default 0° starts from the right of the device.)
+  const _degree = (magnetometer: any) => {
+    return magnetometer - 90 >= 0 ? magnetometer - 90 : magnetometer + 271;
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Text style={styles.text}>Magnetometer:</Text>
+      <Text style={styles.text}>x: {x}</Text>
+      <Text style={styles.text}>y: {y}</Text>
+      <Text style={styles.text}>z: {z}</Text>
+
+      <Text>{_degree(magnetometer)}</Text>
+      <Text>{_direction(_degree(magnetometer))}</Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={subscription ? _unsubscribe : _subscribe}
+          style={styles.button}
+        >
+          <Text>{subscription ? "On" : "Off"}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={_slow}
+          style={[styles.button, styles.middleButton]}
+        >
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -16,16 +125,27 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingHorizontal: 10,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  text: {
+    textAlign: "center",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#ccc",
   },
 });
