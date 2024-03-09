3bd08609eb42c0536f0e4dd08525ed850619b523
Martin Brozek <martin.brozek@yoso.fi>
2024-03-09 22:15:02 +0100

    feat: contacts page

--- fe/blind-tooth-fe/app/(tabs)/contacts.tsx
+++ fe/blind-tooth-fe/app/(tabs)/contacts.tsx
@@ -1,11 +1,78 @@
-import { StyleSheet } from "react-native";
+import {ScrollView, StyleSheet, TouchableOpacity} from "react-native";
 
 import { Text, View } from "@/components/Themed";
+import {Button, Searchbar, Surface} from "react-native-paper";
+import React, {useEffect, useState} from "react";
+import Calls from "@/utils/api/client";
+import {ResponseData} from "@/utils/cache";
+import {useAsyncStorage} from "@react-native-async-storage/async-storage";
+import {REGISTRATION_KEY} from "@/app/register";
+
+type userId = {
+  userId: string
+}
 
 export default function App() {
+  const { getItem } = useAsyncStorage(REGISTRATION_KEY);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const [contacts, setContacts] = useState<userId[]>([]);
+
+  useEffect(() => {
+    getItem().then((res) => {
+      setUserId(res);
+    });
+  }, []);
+
+  async function fetchContacts(){
+    console.log(searchQuery);
+    let response;
+    try {
+      response = await Calls.post<any>(
+          "https://hog9cqd8ol.execute-api.eu-west-1.amazonaws.com/default/getUsers",
+          {},
+          {params: {userId: searchQuery}}
+      );
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+
+    if (response.status !== 200) {
+      console.log("Error fetching data");
+      return;
+    }
+
+    const users = response.data as userId[];
+    console.log("Contacts: " + users);
+    setContacts(users);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Contacty</Text>
+      <Searchbar
+          style={styles.searchBar}
+          placeholder="Hledej uzivatele"
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+          onBlur={fetchContacts}
+      />
+
+    <ScrollView >
+      {contacts.map((contact) => <Surface key={contact.userId} style={styles.surface} elevation={4}>
+        <Button icon="phone" mode="contained" onPress={() => console.log('Calling'+ contact)}>
+          Volat
+        </Button>
+        <Text style={styles.text}>
+        {contact.userId}
+        </Text>
+        <Button icon="human-greeting-proximity" mode="contained" onPress={() => console.log('Pressed')}
+                contentStyle={{flexDirection: 'row-reverse'}}
+        >
+          Propojit
+        </Button>
+      </Surface>)}
+    </ScrollView>
     </View>
   );
 }
@@ -13,7 +80,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingVertical: 25,
     paddingHorizontal: 10,
   },
+  text: {
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#ccc",
+  },
+  surface: {
+    flex: 1,
+    flexDirection: "row",
+    padding: 8,
+    height: 80,
+    width: "100%",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 25,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  searchBar:{
+    marginBottom: 10
+  }
 });
