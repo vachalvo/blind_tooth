b1471efcaf5ffad10f2b86fa5b163ec693f56435
nikimasek <niki.masek@icloud.com>
2024-03-09 13:39:59 +0100

    Update vibration.tsx

--- fe/blind-tooth-fe/app/(tabs)/vibration.tsx
+++ fe/blind-tooth-fe/app/(tabs)/vibration.tsx
@@ -4,45 +4,48 @@ import { Text, View } from "@/components/Themed";
 import React from "react";
 
 export default function App() {
-  const ONE_SECOND_IN_MS = 500;
 
-  const PATTERN = [
-    1 * ONE_SECOND_IN_MS,
-    2 * ONE_SECOND_IN_MS,
-    3 * ONE_SECOND_IN_MS,
-    4 * ONE_SECOND_IN_MS,
-  ];
+  const pattern_sym_short_one_shot: number[] = [80, 80];
+  const pattern_sym_short_two_shot: number[] = [...pattern_sym_short_one_shot, ...pattern_sym_short_one_shot];
+  const pattern_sym_short_thee_shot: number[] = [...pattern_sym_short_one_shot, ...pattern_sym_short_one_shot, ...pattern_sym_short_one_shot];
+
+  const pattern_sym_long_one_shot: number[] = [200, 400];
+  const pattern_sym_long_two_shot: number[] = [...pattern_sym_long_one_shot, ...pattern_sym_long_one_shot];
+  const pattern_sym_long_thee_shot: number[] = [...pattern_sym_long_one_shot, ...pattern_sym_long_one_shot, ...pattern_sym_long_one_shot];
+
+  const error_pattern: number[] = [100, 300, 80, 80, 80, 80, 200, 400, 200, 400];
+  const wrong_way_pattern_singletime: number[] = [80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80];
+  const wrong_way_pattern: number[] = [...wrong_way_pattern_singletime, ...wrong_way_pattern_singletime];
 
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={() => Vibration.vibrate(250)} style={styles.button}>
-          <Text>.25s</Text>
+        <TouchableOpacity onPress={() => Vibration.vibrate(wrong_way_pattern_singletime)} style={styles.button}>
+          <Text>wrong way</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => Vibration.vibrate(500)} style={[styles.button, styles.middleButton]}>
-          <Text>.5s</Text>
+        <TouchableOpacity onPress={() => Vibration.vibrate(pattern_sym_short_one_shot)} style={[styles.button, styles.middleButton]}>
+          <Text>short1</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => Vibration.vibrate([250, 500, 500, 500, 1000, 500], true)} style={[styles.button, styles.middleButton]}>
-          <Text>test1</Text>
+        <TouchableOpacity onPress={() => Vibration.vibrate(pattern_sym_short_two_shot)} style={[styles.button, styles.middleButton]}>
+          <Text>short2</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => Vibration.vibrate([100, 50, 100, 50, 100, 50])} style={styles.button}>
-          <Text>test2</Text>
+        <TouchableOpacity onPress={() => Vibration.vibrate(pattern_sym_short_thee_shot)} style={styles.button}>
+          <Text>short3</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={() => Vibration.vibrate(PATTERN)} style={styles.button}>
-          <Text>Test</Text>
+        <TouchableOpacity onPress={() => Vibration.vibrate(error_pattern)} style={styles.button}>
+          <Text>error</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => Vibration.vibrate()}
-          style={[styles.button, styles.middleButton]}
-        >
-          <Text>star</Text>
+        <TouchableOpacity onPress={() => Vibration.vibrate(pattern_sym_long_one_shot)} style={[styles.button, styles.middleButton]}>
+          <Text>long1</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => Vibration.cancel()} style={styles.button}>
-          <Text>stop</Text>
+        <TouchableOpacity onPress={() => Vibration.vibrate(pattern_sym_long_two_shot)} style={[styles.button, styles.middleButton]}>
+          <Text>long2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Vibration.vibrate(pattern_sym_long_thee_shot)} style={styles.button}>
+          <Text>long3</Text>
         </TouchableOpacity>
       </View>
     </View>
